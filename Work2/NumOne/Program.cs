d9900b44c902c57d4eef0fdebfafad6c2376c526
1340911984 <63380204+1340911984@users.noreply.github.com>
2021-03-10 11:22:32 +0800

    Add files via upload

---
+++ Work2/NumOne/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace NumOne
+{
+    class Program
+    {
+        public static String GetPrime(int input)
+        {
+            if (input == 1 && input == 0)
+            {
+                return null;
+            }
+            String res = null;
+            for (int i = 2; i*i <= input; i++)
+            {
+                while (input % i == 0)
+                {
+                    res = res + i.ToString() + ",";
+                    input /= i;
+                }
+            }
+            return res + input.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            // 课题1
+            Console.WriteLine("**********课题1**********");
+            Console.Write("请输入您想要的求的素数因子： ");
+            String input1 = Console.ReadLine();
+            try
+            {
+                String output1 = GetPrime(int.Parse(input1));
+                Console.WriteLine($"{input1}的素数因子是1,{output1}\n\n");
+            }
+            catch
+            {
+                Console.WriteLine("输入错误\n\n");
+            }
+        }
+    }
+}
---
+++ Work2/Work2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Work2{
+    class Program {
+
+       
+        public static Tuple<int, int ,int> GetMaxMinSum(int[] nums)
+        {
+            if (nums.Length == 0) { return null; }
+            int max = nums[0];
+            int min = nums[0];
+            int sum = 0;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] > max)
+                {
+                    max = nums[i];
+                }
+                else if (nums[i] < min)
+                {
+                    min = nums[i];
+                }
+                sum += nums[i];
+            }
+            return Tuple.Create(max, min, sum); //
+        }
+  
+        static void Main(string[] args) {
+            //课题2
+            Console.WriteLine("**********课题2**********");
+            int[] nums = { 4, 5, 6, 1, 3, 7, 71, 3, 4, 129 };
+            var ans =  GetMaxMinSum(nums);
+            float ave =  ans.Item3/(float)(nums.Length);
+            Console.WriteLine($"最大值：{ans.Item1}，最小值：{ans.Item2}，求和:{ans.Item3}，平均值:{ave}\n\n");
+
+
+            
+/*Main*/
+        }
+/*Class*/}
+/*NameSpace*/}
