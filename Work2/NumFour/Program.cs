053d6422c405ba72dc5945bc1f826bb78288c0d6
1340911984 <63380204+1340911984@users.noreply.github.com>
2021-03-10 11:23:38 +0800

    Add files via upload

---
+++ Work2/NumFour/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace NumFour
+{
+    class Program
+    {
+        public static Boolean JudgeTheMartix(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (row * col != 0 && matrix[row, col] != matrix[row - 1, col - 1])
+                        return false;
+                }
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            //课题4
+            Console.WriteLine("**********课题4**********");
+            int[,] martix = { { 1, 2, 3, 4 }, { 5, 1, 2, 3 }, { 9, 5, 1, 2 }, };
+            if (JudgeTheMartix(martix))
+            {
+                Console.WriteLine("是符合托普利茨的矩阵 ");
+            }
+            else
+            {
+                Console.WriteLine("不符合 ");
+            }
+        }
+    }
+}
---
+++ Work2/NumThree/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace NumThree
+{
+    class Program
+    {
+        public static bool[] GetPrimeWithFunct(int length)
+        {
+            Boolean[] ans = new Boolean[length + 1];
+            int MinPrime = 2;
+            while (MinPrime * MinPrime < ans.Length - 1)
+            {
+                for (int i = MinPrime*2 ; i < ans.Length; i+=MinPrime)
+                {
+                    if (i % MinPrime == 0)
+                        ans[i] = true;
+                }
+
+                for (int i = 1; i < ans.Length; i++)
+                {
+                    if (i > MinPrime && ans[i] == false)
+                    {
+                        MinPrime = i;
+                        break;
+                    }
+                }
+            }
+            return ans;
+        }
+
+        static void Main(string[] args)
+        {
+            //课题3
+            Console.WriteLine("**********课题3**********");
+            Boolean[] ans = GetPrimeWithFunct(100);//从2到100
+            for (int i = 2; i < ans.Length; i++)
+            {
+                if (ans[i] == false) { Console.Write($"{i},"); }
+                if (i % 10 == 0) { Console.WriteLine(); }
+            }
+            Console.WriteLine("\n");
+
+
+        }
+    }
+}
