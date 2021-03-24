cebc0ce318e986ddec6f3f48aab073aff0de14a1
1340911984 <63380204+1340911984@users.noreply.github.com>
2021-03-24 11:02:43 +0800

    Update Form1.cs

--- Work4/Work4_2/Form1.cs
+++ Work4/Work4_2/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,7 +13,8 @@ namespace Work4_2
     public partial class Form1 : Form
     {
         DateTime now = DateTime.UtcNow;
-       
+        int Tcik__num = 0;
+        int Alarm_num = 0;
         public Form1()
         {
             InitializeComponent();
@@ -26,18 +27,20 @@ namespace Work4_2
         private void Tick_Click(object sender, EventArgs e)
         {
             Console.WriteLine(this.Time.Text);
-            this.message.Text = "控制台信息：" + System.DateTime.Now.ToString("我在 HH时-mm分 Tick了一下");
+            Tcik__num++;
+            this.message.Text = "控制台信息：" + System.DateTime.Now.ToString("我在 mm分-ss秒 ") + "Tick了第"+ Tcik__num + "下";
         }
 
         private void Alarm_Click(object sender, EventArgs e)
         {
             Console.WriteLine("闹钟响了");
-            this.message.Text = "控制台信息：" + System.DateTime.Now.ToString("我在 HH时-mm分 ") + "Alarm了一下";
+            Alarm_num++;
+            this.message.Text = "控制台信息：" + System.DateTime.Now.ToString("我在 mm分-ss秒") + "Alarm了第" + Alarm_num + "下"; 
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            this.Time.Text = "时间：" + System.DateTime.Now.ToString("yyyy年-MM月-dd日-HH时-mm分-ss秒");
+            this.Time.Text ="时间：" + System.DateTime.Now.ToString("yyyy年-MM月-dd日-HH时-mm分-ss秒");
         }
 
         private void textBox1_TextChanged_1(object sender, EventArgs e)
