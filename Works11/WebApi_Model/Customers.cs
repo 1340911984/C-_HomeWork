fbb8d0f55833a58b9519223f94d67e8187b453ce
1340911984 <63380204+1340911984@users.noreply.github.com>
2021-06-07 18:42:36 +0800

    Add files via upload

---
+++ Works11/WebApi_Model/Customers.cs
@@ -0,0 +1,52 @@
+﻿using System;
+namespace WebApi_Model
+{
+	/// <summary>
+	/// Customers:实体类(属性说明自动提取数据库字段的描述信息)
+	/// </summary>
+	[Serializable]
+	public partial class Customers
+	{
+		public Customers()
+		{}
+		#region Model
+		private int _kid;
+		private string _customername;
+		private string _tel;
+		/// <summary>
+		/// 
+		/// </summary>
+		public int Kid
+		{
+			set{ _kid=value;}
+			get{return _kid;}
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		public string CustomerName
+		{
+			set{ _customername=value;}
+			get{return _customername;}
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		public string Tel
+		{
+			set{ _tel=value;}
+			get{return _tel;}
+		}
+		#endregion Model
+	}
+
+    [Serializable]
+    public class CustomersList
+    {
+        /// <summary>
+        /// 删除用户列表，用逗号,间隔
+        /// </summary>
+        public string kidList { get; set; }
+    }
+}
+
